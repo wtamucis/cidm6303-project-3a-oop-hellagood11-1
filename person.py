25e0d8c3851d3c864bf7a77077f7afef4a6b5572
Ronan <rhella@uccs.edu>
2026-04-20 19:05:29 -0500

    Compoleted Simple Person Class Task

--- person.py
+++ person.py
@@ -1,1 +1,17 @@
 # Create a simple class to represent a person
+class Person():
+    def __init__(self, first, last):
+        self.first_name = first
+        self.last_name = last
+
+    def speak(self):
+        print(f"Hi. My name is {self.first_name} {self.last_name}.")
+
+p1 = Person("George", "Washington")
+p1.speak()
+
+p2 = Person("John", "Adams")
+p2.speak()
+
+p3 = Person("Thomas", "Jefferson")
+p3.speak()
